b42878349184c55f0e2e0b0e4abae41395625185
josemunoz <jose.e.munoz.nunez@gmail.com>
2025-06-18 21:16:24 -0400

    Nueva mini app, calculadora de IMC

---
+++ app/src/main/java/com/example/myapplicationconxml/MenuActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapplicationconxml
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnSaludarApp = findViewById<Button>(R.id.btnSaludarApp)
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+
+
+        btnSaludarApp.setOnLongClickListener {
+            navigateToSaludarApp()
+            true
+        }
+
+        btnIMCApp.setOnClickListener {
+            navigateToCalculatorIMCApp()
+            true
+        }
+
+    }
+
+    private fun navigateToSaludarApp() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private fun navigateToCalculatorIMCApp(){
+        val intent = Intent(this, CalculatorIMC::class.java )
+        startActivity(intent)
+    }
+}
