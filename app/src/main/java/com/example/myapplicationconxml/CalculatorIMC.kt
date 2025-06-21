ac0b2c5ea4d642b67b5055e869de0f4f772b5737
josemunoz <jose.e.munoz.nunez@gmail.com>
2025-06-21 00:30:05 -0400

    se crean 2 cardView para peso y edad mas botones flotantes, y boton de calcular

--- app/src/main/java/com/example/myapplicationconxml/CalculatorIMC.kt
+++ app/src/main/java/com/example/myapplicationconxml/CalculatorIMC.kt
@@ -1,19 +1,26 @@
 package com.example.myapplicationconxml
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.RangeSlider
 
 class CalculatorIMC : AppCompatActivity() {
 
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
 
+
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,7 @@ class CalculatorIMC : AppCompatActivity() {
 
         initComponents()
         initListeners()
+        initUI()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -34,15 +42,53 @@ class CalculatorIMC : AppCompatActivity() {
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
     }
 
     private fun initListeners() {
         viewMale.setOnClickListener {
-
+            changeGender()
+            setGenderColor()
         }
 
         viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
 
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = "$result cm"
         }
     }
+
+    private fun changeGender(){
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun setGenderColor(){
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent:Boolean) :Int{
+        val colorReference = if(isSelectedComponent){
+             R.color.Morado_profesional
+        }else{
+           R.color.white
+        }
+
+       return ContextCompat.getColor(this, colorReference)
+
+    }
+
+    private fun initUI(){
+        setGenderColor()
+    }
 }
+
+
+
